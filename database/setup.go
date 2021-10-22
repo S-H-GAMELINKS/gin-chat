40297176280950575e53dfdcfd3516563f3f1d7f
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-10-23 02:19:46 +0900

    Add sqlite3 database connection setup code

---
+++ database/setup.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func SetUpDB() (conn *gorm.DB, err error) {
+	conn, err = gorm.Open(sqlite.Open("./database/database.sqlite3"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	db, err := conn.DB()
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	if err != nil {
+		db.Close()
+	}
+
+	return conn, err
+}
