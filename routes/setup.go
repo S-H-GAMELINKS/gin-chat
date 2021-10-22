d4139cb6e40fdbcf766581166291e2906024b4ac
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-10-23 02:46:31 +0900

    Using controller for routes

--- routes/setup.go
+++ routes/setup.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	"github.com/S-H-GAMELINKS/gin-chat/controller"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetUpRoutes(router *gin.Engine, database *gorm.DB) {
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, "Hello world!")
-	})
+	interactor := controller.NewInteractor(database)
+
+	helloController := interactor.NewHelloControllerInstance(database)
+
+	router.GET("/", helloController.Index)
 }
